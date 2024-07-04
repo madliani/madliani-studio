12022dfcb2e85f98540d5d54b35453ae1b8ca875
madliani <madliani@hotmail.com>
2024-07-04 16:55:06 +0400

    refactor: about page refactored

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,64 +7,56 @@ export const About = () => {
         <>
             <SEO title="About us" />
             <Layout>
-                <div>
-                    <Info
-                        subtitle="Welcome to MADLIANI STUDIO!"
-                        title="About us"
-                    >
-                        <p>
-                            madliani is a visionary mobile game and app
-                            development studio dedicated to crafting immersive
-                            digital experiences that captivate and inspire users
-                            globally.
-                        </p>
+                <Info subtitle="Welcome to MADLIANI STUDIO!" title="About us">
+                    <p>
+                        madliani is a visionary mobile game and app development
+                        studio dedicated to crafting immersive digital
+                        experiences that captivate and inspire users globally.
+                    </p>
 
-                        <h2>Our Commitment</h2>
-                        <p>
-                            At madliani, we blend creativity with cutting-edge
-                            technology to deliver innovative solutions tailored
-                            to meet the unique needs of our clients. Whether
-                            it&apos;s developing engaging mobile games or
-                            functional applications, we strive for excellence in
-                            every project.
-                        </p>
+                    <h2>Our Commitment</h2>
+                    <p>
+                        At madliani, we blend creativity with cutting-edge
+                        technology to deliver innovative solutions tailored to
+                        meet the unique needs of our clients. Whether it&apos;s
+                        developing engaging mobile games or functional
+                        applications, we strive for excellence in every project.
+                    </p>
 
-                        <h2>What Sets Us Apart</h2>
-                        <p>
-                            With a passionate team of developers, designers, and
-                            strategists, madliani stands at the forefront of
-                            mobile development. We leverage the latest trends
-                            and tools to create compelling user interfaces,
-                            seamless user experiences, and scalable solutions.
-                        </p>
+                    <h2>What Sets Us Apart</h2>
+                    <p>
+                        With a passionate team of developers, designers, and
+                        strategists, madliani stands at the forefront of mobile
+                        development. We leverage the latest trends and tools to
+                        create compelling user interfaces, seamless user
+                        experiences, and scalable solutions.
+                    </p>
 
-                        <h2>Our Approach</h2>
-                        <p>
-                            We believe in collaborative partnerships and
-                            client-centric solutions. From concept to
-                            deployment, we work closely with our clients to
-                            ensure that our products not only meet but exceed
-                            expectations. Our agile methodology allows us to
-                            adapt swiftly to changes and deliver results
-                            efficiently.
-                        </p>
+                    <h2>Our Approach</h2>
+                    <p>
+                        We believe in collaborative partnerships and
+                        client-centric solutions. From concept to deployment, we
+                        work closely with our clients to ensure that our
+                        products not only meet but exceed expectations. Our
+                        agile methodology allows us to adapt swiftly to changes
+                        and deliver results efficiently.
+                    </p>
 
-                        <h2>Get in Touch</h2>
-                        <p>
-                            Ready to transform your ideas into reality? Contact
-                            madliani today to explore how we can help you
-                            achieve your goals in the dynamic world of mobile
-                            game and app development.
-                        </p>
+                    <h2>Get in Touch</h2>
+                    <p>
+                        Ready to transform your ideas into reality? Contact
+                        madliani today to explore how we can help you achieve
+                        your goals in the dynamic world of mobile game and app
+                        development.
+                    </p>
 
-                        <p>
-                            Email us at{" "}
-                            <a href="mailto:madliani.gamdev@hotmail.com">
-                                madliani.gamdev@hotmail.com
-                            </a>
-                        </p>
-                    </Info>
-                </div>
+                    <p>
+                        Email us at{" "}
+                        <a href="mailto:madliani.gamdev@hotmail.com">
+                            madliani.gamdev@hotmail.com
+                        </a>
+                    </p>
+                </Info>
             </Layout>
         </>
     );
