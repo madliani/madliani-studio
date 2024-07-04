636e404ca8a81f21b9a585c829850925d40a22fd
madliani <madliani@hotmail.com>
2024-07-04 16:23:39 +0400

    fix: layout fixed

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -16,69 +16,7 @@ const Main = styled.main`
 const options: ISourceOptions = {
     detectRetina: true,
     fpsLimit: 120,
-    interactivity: {
-        events: {
-            onClick: {
-                enable: true,
-                mode: "push"
-            },
-            onHover: {
-                enable: true,
-                mode: "repulse"
-            },
-            resize: true
-        },
-        modes: {
-            push: {
-                quantity: 4
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4
-            }
-        }
-    },
-    particles: {
-        collisions: {
-            enable: true
-        },
-        color: {
-            value: "#ffffff"
-        },
-        links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1
-        },
-        move: {
-            direction: "none",
-            enable: true,
-            outModes: {
-                default: "bounce"
-            },
-            random: false,
-            speed: 3,
-            straight: false
-        },
-        number: {
-            density: {
-                area: 800,
-                enable: true
-            },
-            value: 80
-        },
-        opacity: {
-            value: 0.5
-        },
-        shape: {
-            type: "square"
-        },
-        size: {
-            value: { max: 5, min: 1 }
-        }
-    }
+    preset: "links"
 };
 
 export const Layout = ({ children }: Properties) => {
