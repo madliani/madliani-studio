9b7781342319641f1a394db61d1cdee206159d3d
madliani <madliani@hotmail.com>
2024-07-04 16:56:48 +0400

    refactor: projects refactored

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,24 +2,22 @@ import { ProjectItem } from "./ProjectItem";
 
 export const Projects = () => {
     return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <h1 className="section-title">
-                            <span className="main-color">Projects</span>
-                        </h1>
-                    </div>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1 className="section-title">
+                        <span className="main-color">Projects</span>
+                    </h1>
                 </div>
-                <div className="row text-center">
-                    <ProjectItem
-                        imgAlt="Snowfall logo"
-                        imgSrc="https://raw.githubusercontent.com/madliani/snowfall-website/main/assets/icons/logo.png"
-                        projectDescription="A delightful game where you can experience the magic of snowflakes with a simple click."
-                        title="Snowfall Game"
-                        viewSite="https://snowfall-website.vercel.app"
-                    />
-                </div>
+            </div>
+            <div className="row text-center">
+                <ProjectItem
+                    imgAlt="Snowfall logo"
+                    imgSrc="https://raw.githubusercontent.com/madliani/snowfall-website/main/assets/icons/logo.png"
+                    projectDescription="A delightful game where you can experience the magic of snowflakes with a simple click."
+                    title="Snowfall Game"
+                    viewSite="https://snowfall-website.vercel.app"
+                />
             </div>
         </div>
     );
